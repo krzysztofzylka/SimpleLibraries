6b858035967c35e16cf608aca4dca872c1659a90
krzysztofzylka <krzysztofzylka@yahoo.com>
2022-12-14 20:52:03 +0100

    poprawki

--- src/Library/Client.php
+++ src/Library/Client.php
@@ -8,7 +8,7 @@ class Client {
      * Get Client IP address
      * @return string
      */
-    public static function getIp() : string {
+    public static function getIP() : string {
         if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
             return $_SERVER['HTTP_CLIENT_IP'];
         } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
