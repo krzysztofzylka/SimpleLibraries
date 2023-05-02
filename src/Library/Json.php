fe70b8dcca25c72b9c8f97a1b1e8d67efcf0e939
krzysztof.zylka <krzysztof.zylka@wfirma.pl>
2023-05-02 06:55:10 +0200

    Json library

---
+++ src/Library/Json.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace krzysztofzylka\SimpleLibraries\Library;
+
+/**
+ * Json
+ */
+class Json {
+
+    /**
+     * Is json
+     * @param string $string
+     * @return bool
+     */
+    public static function isJson(string $string): bool
+    {
+        json_decode($string);
+
+        return json_last_error() === JSON_ERROR_NONE;
+    }
+
+}
