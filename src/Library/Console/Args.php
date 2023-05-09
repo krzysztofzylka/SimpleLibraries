0c856f2fb8b44b09d6556ccef25a2005030f4ec4
krzysztof.zylka <krzysztof.zylka@wfirma.pl>
2023-05-09 21:41:39 +0200

    Console args

---
+++ src/Library/Console/Args.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace krzysztofzylka\SimpleLibraries\Library\Console;
+
+/**
+ * Arguments reader
+ */
+class Args {
+
+    /**
+     * Get args in readable array
+     * @param array $argv
+     * @return array
+     */
+    public function getArgs(array $argv) : array {
+        $return = [
+            'path' => $argv[0],
+            'args' => [],
+            'params' => []
+        ];
+
+        $isParam = false;
+        $paramName = '';
+
+        foreach ($argv as $i => $arg) {
+            if ($i === 0) {
+                continue;
+            }
+
+            if (str_starts_with($arg, '-')) {
+                $isParam = true;
+                $paramName = substr($arg, 1);
+            } elseif ($isParam) {
+                $isParam = false;
+                $return['params'][$paramName] = $arg;
+                $paramName = '';
+            } else {
+                $return['args'][] = $arg;
+            }
+        }
+
+        return $return;
+    }
+
+}
