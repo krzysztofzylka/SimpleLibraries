f423851af712389c6923c8fd7daa35a1bfc43b01
krzysztof <krzysztofzylka@yahoo.com>
2023-11-25 08:39:05 +0100

    Integer - strToFloat method moved to Math library

--- src/Library/Integer.php
+++ src/Library/Integer.php
@@ -8,19 +8,6 @@ namespace krzysztofzylka\SimpleLibraries\Library;
 class Integer {
 
     /**
-     * Convert string to float
-     * @param string $int
-     * @return float
-     */
-    public static function strToFloat(string $int) : float {
-        if (str_contains($int, '.') && str_contains($int, ',')) {
-            $int = str_replace(',', '', $int);
-        }
-
-        return floatval(str_replace(',', '.', trim($int)));
-    }
-
-    /**
      * String is integer
      * @param $string
      * @return bool
