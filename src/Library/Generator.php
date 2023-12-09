b5eaf221952b65583a0f508b77464b974a85c2a5
Krzysztof Żyłka <krzysztofzylka@yahoo.com>
2023-12-09 10:19:25 +0100

    Generator Methods uniqId, uniqHash and guid moved to https://packagist.org/packages/krzysztofzylka/generator

--- src/Library/Generator.php
+++ src/Library/Generator.php
@@ -11,37 +11,6 @@ use Exception;
 class Generator {
 
     /**
-     * Unikatowy identyfikator
-     * @param ?int $length length to 512
-     * @return string
-     * @throws Exception
-     */
-    public static function uniqId(?int $length = null) : string {
-        $uniqId = str_pad(random_int(1, 99999), 5, 0, STR_PAD_LEFT);
-        $uniqId .= time();
-        $uniqId .= str_replace('.', '', uniqid('', true));
-        $uniqId .= uniqid();
-
-        $uniqId = hash('sha512', $uniqId);
-
-        if (!is_null($length)) {
-            $uniqId = substr($uniqId, 0, $length);
-        }
-
-        return $uniqId;
-    }
-
-    /**
-     * Unikatowy identyfikator (hash xxh128)
-     * @param string $salt
-     * @return string
-     * @throws Exception
-     */
-    public static function uniqHash(string $salt = '') : string {
-        return hash('xxh128', $salt . self::uniqId());
-    }
-
-    /**
      * Int to bytes
      * @param int $bytes
      * @param int $precision
@@ -56,19 +25,6 @@ class Generator {
         }
 
         return sprintf("%.{$precision}f %s", $bytes / (1024 ** $factor), $size[$factor]);
-    }
-
-    /**
-     * GUID generator
-     * @return string
-     * @throws Exception
-     */
-    public static function guid() : string {
-        $data = random_bytes(16);
-        $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
-        $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
-
-        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
     }
 
 }
